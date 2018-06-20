238814186b197122f42b35a2596cd536bc810f52
claudio <cmargleon@gmail.com>
2018-06-20 14:44:25 -0400

    2

--- network.js
+++ network.js
@@ -1,4 +1,21 @@
+const AdminConnection = require('composer-admin').AdminConnection;
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
 
+//declate namespace
+const namespace = 'org.degree.ucsd';
+
+//in-memory card store for testing so cards are not persisted to the file system
+const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
+
+//admin connection to the blockchain, used to deploy the business network
+let adminConnection;
+
+//this is the business network connection the tests will use.
+let businessNetworkConnection;
+
+let businessNetworkName = 'degree';
+let factory;
 //export module
 module.exports = {
 
