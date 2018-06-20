5de9969279454c2ed63c3a27fb8c12253a39ba54
claudio <cmargleon@gmail.com>
2018-06-20 14:51:28 -0400

    2

--- index.js
+++ index.js
@@ -3,26 +3,24 @@
 //get libraries
 const express = require('express');
 const bodyParser = require('body-parser');
+//const request = require('request');
 const path = require('path');
 
 //create express web-app
 const app = express();
 const router = express.Router();
 
+//call libraries to get
+var validate = require('./validate.js');
 var network = require('./network.js');
 
 //bootstrap app
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
 
-app.get('/test', function(req,res) {
-    res.send("Funciona")
-})
-
 //post call to register graduate on the network
 app.post('/api/registerGraduate', function(req, res) {
     console.log("Creando cuenta graduado");
-    res.sendStatus(200)
     var graduateRut = req.body.graduaterut;
     var cardId = req.body.cardid;
     var firstName = req.body.firstname;
@@ -31,10 +29,275 @@ app.post('/api/registerGraduate', function(req, res) {
     var phoneNumber = req.body.phonenumber;
 
     console.log(req.body);
+
+    validate.validateGraduateRegistration(graduateRut, cardId, firstName, lastName, phoneNumber, email)
+        .then((response) => {
+            if (response.error != null) {
+                res.json({
+                  error: response.error
+                });
+                return;
+              } else {
+                    //else register graduate on the network
+                network.registerGraduate(cardId, graduateRut, firstName, lastName, email, phoneNumber)
+                .then((response) => {
+                    //return error if error in response
+                    if (response.error != null) {
+                    res.json({
+                        error: response.error
+                    });
+                    } else {
+                    //else return success
+                    res.json({
+                        success: response
+                    });
+                    }
+                });
+              }
+        })
         
 });
 
+//post call to register university on the network
+app.post('/api/registerUniversity', function(req, res) {
+    console.log("Creando cuenta universidad");
+    var universityRut = req.body.universityrut;
+    var cardId = req.body.cardid;
+    var shortName = req.body.shortname;
+    var fullName = req.body.fullname;
+    var email = req.body.email;
 
+    console.log(req.body);
+
+    validate.validateUniversityRegistration(universityRut, cardId, shortName, fullName, email)
+        .then((response) => {
+            if (response.error != null) {
+                res.json({
+                  error: response.error
+                });
+                return;
+              } else {
+                      //else register university on the network
+                network.registerUniversity(cardId, universityRut, shortName, fullName, email)
+                .then((response) => {
+                    //return error if error in response
+                    if (response.error != null) {
+                    res.json({
+                        error: response.error
+                    });
+                    } else {
+                    //else return success
+                    res.json({
+                        success: response
+                    });
+                    }
+                });
+              }
+        })
+});
+
+//post call to register business on the network
+app.post('/api/registerBusiness', function(req, res) {
+    console.log("Creando cuenta negocio");
+    var businessRut = req.body.businessrut;
+    var cardId = req.body.cardid;
+    var shortName = req.body.shortname;
+    var fullName = req.body.fullname;
+    var email = req.body.email;
+
+
+    validate.validateBusinessRegistration(businessRut, cardId, shortName, fullName, email)
+        .then((response) => {
+            if (response.error != null) {
+                res.json({
+                  error: response.error
+                });
+                return;
+              } else {
+                      //else register business on the network
+                      network.registerBusiness(cardId, businessRut, shortName, fullName, email)
+                      .then((response) => {
+                          //return error if error in response
+                          if (response.error != null) {
+                          res.json({
+                              error: response.error
+                          });
+                          } else {
+                          //else return success
+                          res.json({
+                              success: response
+                          });
+                          }
+                      });
+              }
+        })
+});
+
+//post call to log in graduate NO ESTOY SEGURO! REVISAR!
+app.post('/api/logingraduate', function(req,res) {
+    console.log(req.body.graduaterut);
+    //declare variables to retrieve from request
+    var graduateRut = req.body.graduaterut;
+    var cardId = req.body.cardid;
+    //print variables
+  console.log('memberData using param - ' + ' accountNumber: ' + graduateRut + ' cardId: ' + cardId);
+
+  //declare return object
+  var returnData = {};
+
+  //get graduate data from network
+  network.graduateData(cardId, graduateRut)
+    .then((graduate) => {
+      //return error if error in response
+      if (graduate.error != null) {
+          console.log("ERROR!!!")
+        res.json({
+          error: graduate.error
+        });
+      } else {
+        //else add graduate data to return object
+        returnData.graduateRut = graduate.graduateRut;
+        returnData.firstName = graduate.firstName;
+        returnData.lastName = graduate.lastName;
+        returnData.phoneNumber = graduate.phoneNumber;
+        returnData.email = graduate.email;
+        //returnData.points = member.points; REVISAR ESTO!!!!!!
+        console.log(returnData)
+      }
+
+    })
+});
+
+//post call to log in university NO ESTOY SEGURO! REVISAR!
+app.post('/api/loginuniversity', function(req,res) {
+    console.log("iniciando sesión universidad");
+    //declare variables to retrieve from request
+    var universityRut = req.body.universityrut;
+    var cardId = req.body.cardid;
+
+    //print variables
+  console.log('memberData using param - ' + ' accountNumber: ' + universityRut + ' cardId: ' + cardId);
+
+  //declare return object
+  var returnData = {};
+
+  //get university data from network
+  network.universityData(cardId, universityRut)
+    .then((university) => {
+      //return error if error in response
+      if (university.error != null) {
+          console.log("ERROR!!!")
+        res.json({
+          error: university.error
+        });
+      } else {
+        //else add university data to return object
+        returnData.universityRut = university.universityRut;
+        returnData.shortName = university.shortName;
+        returnData.fullName = university.fullName;
+        returnData.email = university.email;
+        //returnData.points = member.points; REVISAR ESTO!!!!!!
+        console.log(returnData)
+      }
+
+    })
+});
+
+//post call to log in business NO ESTOY SEGURO! REVISAR!
+app.post('/api/loginbusiness', function(req,res) {
+    console.log("iniciando sesión business");
+    //declare variables to retrieve from request
+    var businessRut = req.body.businessrut;
+    var cardId = req.body.cardid;
+
+    console.log('memberData using param - ' + ' accountNumber: ' + businessRut + ' cardId: ' + cardId);
+
+  //declare return object
+  var returnData = {};
+
+  //get business data from network
+  network.businessData(cardId, businessRut)
+    .then((business) => {
+      //return error if error in response
+      if (business.error != null) {
+          console.log("ERROR!!!")
+        res.json({
+          error: business.error
+        });
+      } else {
+        //else add business data to return object
+        returnData.businessRut = business.businessRut;
+        returnData.shortName = business.shortName;
+        returnData.fullName = business.fullName;
+        returnData.email = business.email;
+        //returnData.points = member.points; REVISAR ESTO!!!!!!
+        console.log(`returndata: ${returnData}`);
+        res.json({
+          success: returnData
+      });
+      }
+
+    })
+});
+
+app.post('/api/createregistry', function(req, res) {
+  //console.log(req.body.degreeid);
+  
+  var degreeId = req.body.degreeid;
+  var graduateRut = req.body.graduaterut;
+  var owner = req.body.owner;
+  var degreeType = req.body.degreetype;
+  var degreeStatus = req.body.degreestatus;
+  var major = req.body.major;
+  var minor = req.body.minor;
+  var startYear = req.body.startyear;
+  var gradYear = req.body.gradyear;
+  var gpa = req.body.gpa;
+  var cardId = req.body.cardid;
+
+  //cardId = req.body.cardid;
+  var returnData = {};
+  cardId = req.body.cardid
+
+  network.createRegistry(cardId, req.body.degreeid, graduateRut, owner, degreeType, degreeStatus, major, minor, startYear, gradYear, gpa)
+})
+
+app.post('/api/authorizedegree', function(req, res){
+  //console.log(req.body.businessrut)
+  var cardId = req.body.cardid;
+  var businessRut = req.body.businessrut;
+  var degreeId = req.body.degreeid;
+  console.log(businessRut)
+
+  var returnData = {};
+
+  network.authorizeDegree(cardId, businessRut, degreeId)
+  .then(data => console.log(`data: ${data}`))
+});
+
+app.post('/api/authorizefullregistry', function(req, res){
+  //console.log(req.body.businessrut)
+  var cardId = req.body.cardid;
+  var businessRut = req.body.businessrut;
+  var graduate = req.body.graduate;
+  var university = req.body.university;
+  console.log(businessRut)
+
+  var returnData = {};
+
+  network.authorizeFullRegistry(cardId, businessRut, graduate, university)
+});
+
+app.post('/api/queryfullregistry', function(req, res){
+  console.log(req.body)
+  var cardId = req.body.cardid;
+  var graduateRut = req.body.graduaterut;
+
+  var returnData = {};
+
+  network.queryRegistries(cardId, graduateRut)
+  .then(data => console.log(`data: ${data}`))
+});
 
 //declare port
 var port = process.env.PORT || 8000;
--- network.js
+++ network.js
@@ -17,6 +17,12 @@ let businessNetworkConnection;
 let businessNetworkName = 'degree';
 let factory;
 
+
+/*
+ * Import card for an identity
+ * @param {String} cardName The card name to use for this identity
+ * @param {Object} identity The identity details
+ */
 async function importCardForIdentity(cardName, identity) {
 
   //use admin connection
@@ -38,6 +44,7 @@ async function importCardForIdentity(cardName, identity) {
   //import card
   await adminConnection.importCard(cardName, card);
 }
+
 //export module
 module.exports = {
 
@@ -92,4 +99,393 @@ module.exports = {
       }
   
     },
+
+    /*
+    * Create University participant and import card for identity
+    * @param {String} cardId Import card id for university
+    * @param {String} universityRut University account number as identifier on network
+    * @param {String} shortName University short name
+    * @param {String} fullName University short name
+    * @param {String} email University email
+    */
+   registerUniversity: async function (cardId, universityRut,shortName, fullName, email) {
+    try {
+
+      //connect as admin
+      businessNetworkConnection = new BusinessNetworkConnection();
+      await businessNetworkConnection.connect('admin@degree');
+
+      //get the factory for the business network
+      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+      //create university participant
+      const university = factory.newResource(namespace, 'University', universityRut);
+      university.shortName = shortName;
+      university.fullName = fullName;
+      university.email = email;
+      
+
+      //add university participant
+      const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.University');
+      await participantRegistry.add(university);
+
+      //issue identity
+      const identity = await businessNetworkConnection.issueIdentity(namespace + '.University#' + universityRut, cardId);
+
+      //import card for identity
+      await importCardForIdentity(cardId, identity);
+
+      //disconnect
+      await businessNetworkConnection.disconnect('admin@degree');
+
+      return true;
+    }
+    catch(err) {
+      //print and return error
+      console.log(err);
+      var error = {};
+      error.error = err.message;
+      return error;
+    }
+
+  },
+
+  /*
+    * Create Business participant and import card for identity
+    * @param {String} cardId Import card id for business
+    * @param {String} businessRut Business account number as identifier on network
+    * @param {String} shortName Business short name
+    * @param {String} fullName Business short name
+    * @param {String} email Business email
+    */
+   registerBusiness: async function (cardId, businessRut,shortName, fullName, email) {
+    try {
+
+      //connect as admin
+      businessNetworkConnection = new BusinessNetworkConnection();
+      await businessNetworkConnection.connect('admin@degree');
+
+      //get the factory for the business network
+      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+      //create business participant
+      const business = factory.newResource(namespace, 'Business', businessRut);
+      business.shortName = shortName;
+      business.fullName = fullName;
+      business.email = email;
+      
+
+      //add business participant
+      const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Business');
+      await participantRegistry.add(business);
+
+      //issue identity
+      const identity = await businessNetworkConnection.issueIdentity(namespace + '.Business#' + businessRut, cardId);
+
+      //import card for identity
+      await importCardForIdentity(cardId, identity);
+
+      //disconnect
+      await businessNetworkConnection.disconnect('admin@degree');
+
+      return true;
+    }
+    catch(err) {
+      //print and return error
+      console.log(err);
+      var error = {};
+      error.error = err.message;
+      return error;
+    }
+
+  },
+
+  /*
+  * Get Graduate data
+  * @param {String} cardId Card id to connect to network
+  * @param {String} graduateRut Account number of graduate
+  */
+ graduateData: async function (cardId, graduateRut) {
+
+  try {
+
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    await businessNetworkConnection.connect(cardId);
+
+    //get graduate from the network
+    const graduateRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Graduate');
+    const graduate = await graduateRegistry.get(graduateRut);
+
+    //disconnect
+    await businessNetworkConnection.disconnect(cardId);
+    
+
+    //return graduate object
+    return graduate;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    console.log("EORR")
+    var error = {};
+    error.error = err.message;
+    return error;
+  }
+
+},
+
+/*
+  * Get University data
+  * @param {String} cardId Card id to connect to network
+  * @param {String} universityRut Account number of university
+  */
+ universityData: async function (cardId, universityRut) {
+  console.log(`rut: ${universityRut}`)
+  try {
+
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    await businessNetworkConnection.connect(cardId);
+
+    //get university from the network
+    const universityRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.University');
+    const university = await universityRegistry.get(universityRut);
+
+    //disconnect
+    await businessNetworkConnection.disconnect(cardId);
+    
+
+    //return university object
+    return university;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    console.log("EORR")
+    var error = {};
+    error.error = err.message;
+    return error;
+  }
+
+},
+
+/*
+  * Get Business data
+  * @param {String} cardId Card id to connect to network
+  * @param {String} businessRut Account number of business
+  */
+ businessData: async function (cardId, businessRut) {
+  console.log(`rut: ${businessRut}`)
+  try {
+
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    await businessNetworkConnection.connect(cardId);
+
+    //get business from the network
+    const businessRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Business');
+    const business = await businessRegistry.get(businessRut);
+
+    //disconnect
+    await businessNetworkConnection.disconnect(cardId);
+    
+
+    //return business object
+    return business;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    console.log("EORR")
+    var error = {};
+    error.error = err.message;
+    return error;
+  }
+
+},
+
+/*
+  * Create registry
+  * @param {String} cardId Card id to connect to network
+  * @param {String} accountNumber Account number of member
+  * @param {String} partnerId Partner Id of partner
+  * @param {Integer} points Points value
+  */
+ createRegistry: async function (cardId, degreeId, graduateRut, owner, degreeType, degreeStatus, major, minor, startYear, gradYear, gpa) {
+  console.log(degreeType)
+  try {
+
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    businessNetworkDefinition = await businessNetworkConnection.connect(cardId);
+    
+
+    //businessNetworkDefinition = new BusinessNetworkDefinition();
+    
+    degreesRegistry = await businessNetworkConnection.getAssetRegistry(namespace + '.Degree');
+    console.log(degreeType);
+    factory = businessNetworkDefinition.getFactory();
+    console.log(degreeType);
+
+    //get the factory for the business network.
+    
+    var rutString = graduateRut.toString();
+
+    //const degreeRegistry = await businessNetworkConnection.getAssetRegistry(namespace + '.Degree');
+    const owner = factory.newResource(namespace, 'Graduate', rutString);
+
+    let degree2 = factory.newResource(namespace, 'Degree', degreeId);
+    
+
+    
+
+    degree2.owner = "org.degree.ucsd.Graduate#" + rutString;
+    degree2.graduateRut = rutString;
+    degree2.degreeType = degreeType;
+    degree2.degreeStatus = degreeStatus;
+    degree2.major = major;
+    degree2.minor = minor;
+    degree2.startYear = startYear;
+    degree2.gradYear = gradYear;
+    degree2.gpa = gpa;
+    console.log(degree2)
+    await degreesRegistry.addAll([degree2])
+    console.log("aqui")
+
+
+    return true;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    var error = {};
+    error.error = err.message;
+    return error;
+  }
+
+},
+
+/*
+  * Authorize Degree
+  * @param {String} cardId Card id to connect to network
+  * @param {String} businessRut Account number of member
+  * @param {String} degreeId Account number of member
+  */
+ authorizeDegree: async function (cardId, businessRut, degreeId) {
+
+  try {
+
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    await businessNetworkConnection.connect(cardId);
+
+    //get the factory for the business network.
+    factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+    //create transaction
+    const transaction = factory.newTransaction(namespace, 'AuthorizeDegreeAccess');
+    transaction.businessRut = businessRut;
+    transaction.degreeIds = [degreeId];
+
+    console.log(transaction)
+    
+
+    //submit transaction
+    await businessNetworkConnection.submitTransaction(transaction);
+    console.log("Transacción hecha")
+
+    //disconnect
+    await businessNetworkConnection.disconnect(cardId);
+
+    return true;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    var error = {};
+    error.error = err.message;
+    return error
+  }
+
+},
+
+/*
+  * Authorize Full Registry
+  * @param {String} cardId Card id to connect to network
+  * @param {String} businessRut Account number of member
+  */
+ authorizeFullRegistry: async function (cardId, businessRut, graduate, university) {
+
+  try {
+
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    await businessNetworkConnection.connect(cardId);
+
+    //get the factory for the business network.
+    factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+    //create transaction
+    const transaction = factory.newTransaction(namespace, 'AuthorizeAccess');
+    transaction.businessRut = businessRut;
+    transaction.graduate = factory.newRelationship(namespace, 'Graduate', graduate);
+    transaction.university = factory.newRelationship(namespace, 'University', university);
+    transaction.business = factory.newRelationship(namespace, 'Business', businessRut);
+    //transaction.graduate = graduate;
+
+    console.log(transaction)
+    
+
+    //submit transaction
+    await businessNetworkConnection.submitTransaction(transaction);
+    console.log("Transacción hecha");
+
+    //disconnect
+    await businessNetworkConnection.disconnect(cardId);
+
+    return true;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    var error = {};
+    error.error = err.message;
+    return error
+  }
+
+},
+
+/*
+  * Get all registries
+  * @param {String} cardId Card id to connect to network
+  */
+ queryRegistries : async function (cardId, graduateRut) {
+
+  try {
+    //connect to network with cardId
+    businessNetworkConnection = new BusinessNetworkConnection();
+    await businessNetworkConnection.connect(cardId);
+
+    //query all partners from the network
+    const allRegistries = await businessNetworkConnection.query('getDegreeByGraduateRut', { graduateRut: graduateRut});
+
+    //disconnect
+    await businessNetworkConnection.disconnect(cardId);
+
+    console.log(`egistries: ${allRegistries}`)
+
+    //return allPartners object
+    return allRegistries;
+  }
+  catch(err) {
+    //print and return error
+    console.log(err);
+    var error = {};
+    error.error = err.message;
+    return error
+  }
+},
+
+    //AGREGAR MAS FUNCIONES AQUI!
 }
