9fb9b9c8f486f6ece5cd1d85d85441dc8c5de153
claudio <cmargleon@gmail.com>
2018-06-22 14:39:11 -0400

    dsada

--- network.js
+++ network.js
@@ -38,7 +38,17 @@ async function importCardForIdentity(cardName, identity) {
   };
 
   //get connectionProfile from json, create Idcard
-  const connectionProfile = require('./home/claudiomatiasrg/degree/connection.json');
+  let connectionProfile =  { name: 'hlfv1',
+                          "x-type": "hlfv1",
+                          type: 'hlfv1',
+                          orderers: [ { url: 'grpc://localhost:7050' } ],
+                          ca: { url: 'http://localhost:7054', name: 'ca.org1.example.com' },
+                          peers: 
+                           [ { requestURL: 'grpc://localhost:7051',
+                               eventURL: 'grpc://localhost:7053' } ],
+                          channel: 'composerchannel',
+                          mspID: 'Org1MSP',
+                          timeout: 300 };
   const card = new IdCard(metadata, connectionProfile);
 
   //import card
